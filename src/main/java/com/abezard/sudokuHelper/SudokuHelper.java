34e5116f698b597d96e049288b99d0ee61b58cbf
Adam Bezard <a.s.bezard@student.tudelft.nl>
2025-07-16 16:16:40 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/abezard/sudokuHelper/SudokuHelper.java
@@ -0,0 +1,13 @@
+package com.abezard.sudokuHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuHelper {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokuHelper.class, args);
+	}
+
+}
---
+++ src/test/java/com/abezard/sudokuHelper/SudokuHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abezard.sudokuHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
